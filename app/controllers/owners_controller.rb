5486aafd68b5f96a558359ac892446203656fcd8
Ronald Stephens <rstephens@bluemorpho.cc>
2019-11-20 20:53:35 +0000

    Automatically backed up by Learn

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -13,6 +13,7 @@ class OwnersController < ApplicationController
   post '/owners' do
     @owner = Owner.create(params[:owner])
     @owner.pets << Pet.create(name: params["pet"]["name"]) if params["pet"]["name"] != ""
+    binding.pry
     redirect "owners/#{@owner.id}"
   end
 
